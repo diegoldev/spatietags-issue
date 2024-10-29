665c0329254f383dc5bf6ce1674daf82a677d0fa
Diego O. Mac <diego.a.olmedo@gmail.com>
2024-10-28 23:00:27 -0300

    Issue reproduction

---
+++ app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
+
+class Post extends Model
+{
+    use HasTags;
+
+    protected $guarded = ["id"];
+}
